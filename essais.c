6eba96455620ea73f8f0d111e2d8af0b678e26ab
wartt88 <theo.pinchon9@etu.univ-lorraine.fr>
2023-01-16 21:45:46 +0100

    changements

--- essais.c
+++ essais.c
@@ -1,17 +1,21 @@
-//
-// Created by wartt88 on 16/01/23.
-//
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <sys/wait.h>
+
 #define MAXLI 2048
 
 char cmd[MAXLI];
 char path[MAXLI];
 int pathidx;
+char *which(char *cmd);
 void mbash();
+void commandeGenerale();
+
+
+
 int main(int argc, char** argv) {
     while (1) {
         printf("Commande: ");
@@ -22,13 +26,63 @@ int main(int argc, char** argv) {
 }
 
 void mbash() {
-    char *argv[] = {"ls", "-l", "/"};
-    char *envp[] = {NULL};
     pid_t pid = fork();
-    if(pid == 0){
-        execve("/bin/ls", argv, envp);
+    if (pid == 0) {
+        commandeGenerale(cmd);
         exit(0);
-    }else{
+    } else {
         wait(NULL);
     }
 }
+
+char *which(char *cmd) {
+    char *path_env = getenv("PATH");
+    char *paths = strdup(path_env);
+    char *path;
+    char *command_path = NULL;
+    while ((path = strsep(&paths, ":")) != NULL) {
+        size_t length = strlen(path) + strlen(cmd) + 2;
+        command_path = malloc(length);
+        if (!command_path) {
+            printf("Error: Failed to allocate memory\n");
+            exit(1);
+        }
+        snprintf(command_path, length, "%s/%s", path, cmd);
+        if (access(command_path, X_OK) == 0) {
+            return command_path;
+        }
+        free(command_path);
+        command_path = NULL;
+    }
+    printf("Command not found\n");
+    execlp("","");
+}
+
+void commandeGenerale() {
+    char *argv[MAXLI];
+    char *envp[] = {NULL};
+
+    char *cmd_copy = strdup(cmd);
+    cmd_copy = strtok(cmd_copy, "\n");
+    int i = 0;
+    char *token = strtok(cmd_copy, " ");
+    while (token != NULL) {
+        argv[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    argv[i] = NULL;
+
+    char *str = which(argv[0]);
+
+    printf(" :: %s ::",str);
+
+    int exec_result = execve(str, argv, envp);
+    if (exec_result == -1) {
+        perror("Error executing command");
+    }
+    free(cmd_copy);
+    free(str);
+}
+
+
